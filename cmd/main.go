6d684fe9693da7d485b389255cefd474fdf73ece
crisnlopez <cristianpo@live.com.ar>
2022-11-20 15:42:17 -0300

    chore | modify implementation, render lastPosition

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"image/color"
+	"io"
 	"log"
 	"os"
 
@@ -9,37 +11,52 @@ import (
 	"github.com/notnil/chess/image"
 )
 
+var (
+	colorYellow = color.RGBA{255, 255, 0, 1}
+)
+
 func main() {
 	fmt.Println("Init render")
+	// Open Example Game
 	file, err := os.Open("sample.pgn")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Read game
 	pgn, err := chess.PGN(file)
 	if err != nil {
 		log.Fatal(err)
 	}
 	game := chess.NewGame(pgn)
+	moveHistory := game.MoveHistory()
 
-	positionHistory := game.Positions()
-	lastPositionIdx := len(positionHistory) - 1
-	lastPosition := positionHistory[lastPositionIdx]
-
+	// Create output file
 	output, err := os.Create("output.svg")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	lasPositionData, err := lastPosition.MarshalText()
+	// Print moves
+	fmt.Println("Printing moves")
+	err = printMoves(moveHistory, output)
 	if err != nil {
 		panic(err)
 	}
 
-	var pos chess.Position
-	pos.UnmarshalText(lasPositionData)
-	if err := image.SVG(output, pos.Board()); err != nil {
-		log.Fatal(err)
+	// Render last move
+	lastMove := moveHistory[len(moveHistory)-1]
+	err = image.SVG(output, lastMove.PostPosition.Board(), image.MarkSquares(colorYellow, lastMove.Move.S1(), lastMove.Move.S2()))
+	if err != nil {
+		log.Panicf("error image.SVG: %s", err)
 	}
 	fmt.Println("Finish program")
+}
+
+func printMoves(moves []*chess.MoveHistory, writer io.Writer) error {
+	for i := range moves {
+		fmt.Printf("Board position %d: %s\n", i, moves[i].PostPosition.Board().Draw())
+	}
+
+	return nil
 }
